b5cd3ce8a10e66a0e43729d22ff7223554284f83
Toon Sevrin <twan123@live.be>
2017-06-30 19:14:32 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,6 @@ if [ -z "$NEIGHBORS" ]; then
     echo "Need to set STATE"
     exit 1
 fi  
-$neighbors
-for buddy in $NEIGHBORS; do
-  neighbors="$buddy $neighbors"
-done
-neighbors=${neighbors::-1}
 
-exec java -XX:+DisableAttachMechanism -Xmx4g -Xms4g -jar /iri/target/iri-1.2.2.jar -p 14265 -u 14800 -t 15600 -n "$neighbors" --remote --remote-limit-api "addNeighbors, removeNeighbors" "$@"
+
+exec java -XX:+DisableAttachMechanism -Xmx4g -Xms4g -jar /iri/target/iri-1.2.2.jar -p 14265 -u 14800 -t 15600 -n "$NEIGHBORS" --remote --remote-limit-api "addNeighbors, removeNeighbors" "$@"
